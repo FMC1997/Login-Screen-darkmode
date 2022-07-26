85e5245cb7c1b3e3b17be9aaab7b1c74e1ef92e6
FMC1997 <fmcfabio@live.com>
2022-07-26 04:12:15 +0100

    Adicionar animações

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,5 +1,8 @@
 const $html = document.querySelector('html')
 const $checkbox = document.querySelector('#switch')
+
+
 $checkbox.addEventListener('change', function(){
     $html.classList.toggle('dark-mode')
+  
 })
